0ae371a373a78897cbbf28581619c4959ba9c4d2
Nima <nima.afzal@gmail.com>
2019-01-11 15:26:34 +0330

    Updates

---
+++ precache-manifest.22a4fbaa193602ac91aaad090d77c081.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a695b30138abc35248a",
+    "url": "/todo/static/js/runtime~main.8a695b30.js"
+  },
+  {
+    "revision": "0ac948eaddc1810a9950",
+    "url": "/todo/static/js/main.0ac948ea.chunk.js"
+  },
+  {
+    "revision": "fe4e895aec062e6f1361",
+    "url": "/todo/static/js/1.fe4e895a.chunk.js"
+  },
+  {
+    "revision": "0ac948eaddc1810a9950",
+    "url": "/todo/static/css/main.2f6a0e52.chunk.css"
+  },
+  {
+    "revision": "fe4e895aec062e6f1361",
+    "url": "/todo/static/css/1.9648ae42.chunk.css"
+  },
+  {
+    "revision": "338942aadad8acb3d915d1f786c75def",
+    "url": "/todo/index.html"
+  }
+];
